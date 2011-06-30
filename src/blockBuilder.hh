728a6c7b402ff0cb3d727b9e307713e140f1d97d
Thomas Hartman <rokstar83@gmail.com>
2011-06-30 18:52:49 -0400

    Initial Commit

---
+++ src/blockBuilder.hh
@@ -0,0 +1,30 @@
+/*****************************************************************************/
+/* blockBuilder.hh for blockBuilder                                          */
+/* Copyright (c) 2011 Thomas Hartman (rokstar83@gmail.com)                   */
+/*                                                                           */
+/* This program is free software; you can redistribute it and/or             */
+/* modify it under the terms of the GNU General Public License               */
+/* as published by the Free Software Foundation; either version 2            */
+/* of the License, or the License, or (at your option) any later             */
+/* version.                                                                  */
+/*                                                                           */
+/* This program is distributed in the hope that it will be useful,           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of            */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             */
+/* GNU General Public License for more details.                              */
+/*****************************************************************************/
+#ifndef BLOCKBUILDER_HH_
+#define BLOCKBUILDER_HH_
+#include <QMainWindow>
+
+class BlockBuilder : public QMainWindow
+{
+	 Q_OBJECT
+	 public:
+	 BlockBuilder();
+	 ~BlockBuilder();
+	 
+	 private:
+};
+
+#endif//BLOCKBUILDER_HH_
